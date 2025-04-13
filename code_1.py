f477e83c1a7783d6ab0228832d9ec03e4afcd6c8
Nisa <reyyannisa.eksi@std.yeditepe.edu.tr>
2025-04-13 19:54:28 +0300

    code file was changed

--- code_1.py
+++ code_1.py
@@ -1,2 +1,14 @@
+import time
+
 # First line of your first code
 print("Hello world!")
+
+# New line is added
+print("Hey! I'm changing the file!")
+
+for i in range(5,-1,-1):
+    print("--->", i)
+    time.sleep(1)
+
+print("Rocket was launched!")    
+
